0ce498505d486a58514d822ac65a3bae1a9e4230
carlapenalva <carlapenalva123@gmail.com>
2024-02-06 23:32:18 -0300

    act.0.1

--- javaScript/carrito.js
+++ javaScript/carrito.js
@@ -1,6 +1,11 @@
 const productosEnCarrito = JSON.parse(localStorage.getItem("carrito"));
 console.log(productosEnCarrito);
+const contenedorCarritoVacio = document.getElementById("carritoVacio");
+const contenedorCarritoProductos = document.getElementById(
+  "contenedorCarritoProductos"
+);
 
 if (productosEnCarrito) {
+  contenedorCarritoVacio.classList.add("disabled");
 } else {
 }
